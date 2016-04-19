ec2c1bb37ea9149ed836bab00adce82c6569fdc8
fidel <fidel@yafp.de>
2016-04-19 21:46:54 +0200

    20160419 - Updating to version 0.4 - Fixing #11 #12 #13 #14 #15

--- src/interaptive.sh
+++ src/interaptive.sh
@@ -5,8 +5,8 @@
 #											(inspired by yaourt-gui)
 # Author			:yafp
 # URL				:https://github.com/yafp/interAPTive/
-# Date            	:20160418
-# Version         	:0.3
+# Date            	:20160419
+# Version         	:0.4
 # Usage		 		:bash interaptive.sh 	(non-installed)
 #					:interaptive			(installed via Makefile)
 # Notes				:None
@@ -20,7 +20,7 @@
 appName="interAPTive"
 appDescription="An interactive commandline interface for APT"
 appURL="https://github.com/yafp/interAPTive/"
-appVersion="0.3"
+appVersion="0.4"
 appTagline=" $appName $appVersion - $appDescription - ($appURL)"
 
 
@@ -56,8 +56,9 @@ function checkForRootUser() {
 # CHECK FOR APT
 # ------------------------------------------------------
 function checkForApt() {
+	printf "Searching apt...\n"
 	if hash apt 2>/dev/null; then # check for apt
-        printf "Detected apt"
+        printf "\tDetected apt\n"
     else
 		printf "${bold}${red}ERROR${normal}\tUnable to find apt (errno 1).\n\n"
 		printf "\tPlease make sure you are working on a debian-based system.\n"
@@ -67,14 +68,39 @@ function checkForApt() {
 }
 
 
+# ------------------------------------------------------
+# CHECK TERMINAL WINDOW SIZE
+# ------------------------------------------------------
+function checkTerminalSize() {
+	errorCount=0
+
+	# check Lines
+	if (( $lines < 40 )); then
+		printf "\tWindow height is to small\n"
+		errorCount=$((errorCount+1))
+	fi
+
+	# check columns
+	if (( $columns < 110 )); then
+		printf "\tWindow width is to small\n"
+		errorCount=$((errorCount+1))
+	fi
+
+	# check if errors happened
+	if (( $errorCount > 0 )); then
+		pause
+	fi
+}
+
 
 # ------------------------------------------------------
 # PRINT HEAD
 # ------------------------------------------------------
 function printHead {
-	lines=$(tput lines) # not in use so far
-	columns=$(tput cols)
 	clear
+
+	lines=$(tput lines)
+	columns=$(tput cols)
 
 	printf "\n  _)        |               \    _ \ __ __| _)\n"
 	printf "   |    \    _|   -_)   _| _ \   __/    |    | \ \ /  -_)\n"
@@ -88,6 +114,8 @@ function printHead {
 		printf "-"
 	done
 	printf "${normal}\n\n"
+
+	checkTerminalSize
 }
 
 
@@ -96,36 +124,42 @@ function printHead {
 # PRINT COMMAND LIST
 # ------------------------------------------------------
 function printCommandList {
-	# Update'ing
+	# 1x = Update'ing
 	printf " ${bold}Update${normal}\n"
 	printf "  [1] Update package list\t\t\t\t(apt update)\n"
 	printf "  [2] Download and install updates\t\t\t(apt upgrade)\n"
 	printf "  [3] Download and install updates & dependencies\t(apt dist-upgrade)\n\n"
 
-	# search & info
+	# 2x = search & info
 	printf " ${bold}Info${normal}\n"
-	printf "  [4] Search a package\t\t\t\t\t(apt search PKG-NAME)\n"
-	printf "  [5] Show package information\t\t\t\t(apt show PKG-NAME)\n"
-	printf "  [6] Show package version information\t\t\t(apt-cache policy PKG-NAME)\n"
-	printf "  [7] Show installed packages\t\t\t\t(apt list --installed)\n"
-	printf "  [8] Show upgradable packages\t\t\t\t(apt list --upgradable)\n"
-	printf "  [9] Show all packages\t\t\t\t\t(apt list --all-versions)\n\n"
+	printf " [21] Search packages by name\t\t\t\t(apt search PKG-NAME)\n"
+	printf " [22] Package information\t\t\t\t(apt show PKG-NAME)\n"
+	printf " [23] Package version information\t\t\t(apt-cache policy PKG-NAME)\n"
+	printf " [24] Changelog for single package\t\t\t(apt-get changelog PKG-NAME)\n" # Issue 13
+	printf " [25] Dependencies for single package\t\t\t(apt-cache depends PKG-NAME)\n" # Issue 12
+	printf " [26] List installed packages\t\t\t\t(apt list --installed)\n" # Issue 3
+	printf " [27] List upgradable packages\t\t\t\t(apt list --upgradable)\n" # Issue 3
+	printf " [28] List all packages\t\t\t\t\t(apt list --all-versions)\n\n" # Issue 3
 
-	# install
+	# 3x = install
 	printf " ${bold}Install${normal}\n"
-	printf " [10] Install new packages by name\t\t\t(apt install PKG-NAME)\n\n"
+	printf " [31] Install new packages by name\t\t\t(apt install PKG-NAME)\n"
+	printf " [32] Reinstall a packages by name\t\t\t(apt install --reinstall PKG-NAME)\n\n" # Issue 14
 
-	# remove
+	# 4x = remove
 	printf " ${bold}Removal${normal}\n"
-	printf " [11] Remove packages by name\t\t\t\t(apt remove PKG-NAME)\n"
-	printf " [12] Purge packages by name\t\t\t\t(apt purge PKG-NAME)\n"
-	printf " [13] Remove unneeded packages\t\t\t\t(apt-get autoremove)\n\n"
+	printf " [41] Remove packages by name\t\t\t\t(apt remove PKG-NAME)\n"
+	printf " [42] Purge packages by name\t\t\t\t(apt purge PKG-NAME)\n" # Issue 8
+	printf " [43] Remove unneeded packages\t\t\t\t(apt-get autoremove)\n\n"
 
 	# misc
 	printf " ${bold}Misc${normal}\n"
-	printf "  [L] Show apt log\t\t\t\t\t(/var/log/dpkg)\n"
-	printf "  [E] Edit sources\t\t\t\t\t(apt edit-sources)\n"
+	printf "  [L] Show apt log\t\t\t\t\t(/var/log/dpkg)\n" # Issue 10
+	printf "  [E] Edit sources\t\t\t\t\t(apt edit-sources)\n" # Issue 4
 	printf "  [Q] Quit\n\n"
+
+	#printf "Lines = $lines\n"
+	#printf "Columns = $columns\n"
 }
 
 
@@ -176,7 +210,7 @@ function executeAPTCommand() {
 # PAUSE - WAIT FOR INPUT & RESTART INPUT LOOP
 # ------------------------------------------------------
 function pause() {
-	printf "\n ${green}Press ANY key to return to the main interface ...${normal}"
+	printf "\n ${green}Press ANY key to continue ...${normal}"
 	read -n 1
    	printCoreUI
 }
@@ -219,34 +253,44 @@ function printCoreUI {
 				fi
 				;;
 
-			[4])
+			21)
 				read -p " ${green}Searching for: ${normal}" search
 				executeAPTCommand "apt search $search"
 				;;
 
-			[5])
+			22)
 				read -p " ${green}Show info for pkg: ${normal}" search
 				executeAPTCommand "apt show $search"
 				;;
 
-			[6])
+			23)
 				read -p " ${green}Show policy for pkg: ${normal}" search
 				executeAPTCommand "apt-cache policy $search"
 				;;
 
-			[7])
+			24) # Introduced by Issue: #13
+				read -p " ${green}Please enter a package name for changelog: ${normal}" search
+				executeAPTCommand "apt-get changelog $search"
+				;;
+
+			25) # Introduced by Issue: #12
+				read -p " ${green}Please enter a package name for dependencies: ${normal}" search
+				executeAPTCommand "apt-cache depends $search"
+				;;
+
+			26)
 				executeAPTCommand "apt list --installed"
 				;;
 
-			[8])
+			27)
 				executeAPTCommand "apt list --upgradable"
 				;;
 
-			[9])
+			28)
 				executeAPTCommand "apt list --all-versions"
 				;;
 
-			10)
+			31)
 				read -p " ${green}Please enter a package name for installation: ${normal}" search
 				if [[ $rootUser==false ]]; then
 					executeAPTCommand "sudo apt install $search"
@@ -255,7 +299,16 @@ function printCoreUI {
 				fi
 				;;
 
-			11)
+			32)
+				read -p " ${green}Please enter a package name for re-installation: ${normal}" search
+				if [[ $rootUser==false ]]; then
+					executeAPTCommand "sudo apt install --reinstall $search"
+				else
+					executeAPTCommand "apt install --reinstall $search"
+				fi
+				;;
+
+			41)
 				read -p " ${green}Please enter a package name for removal: ${normal}" search
 				if [[ $rootUser==false ]]; then
 					executeAPTCommand "sudo apt remove $search"
@@ -264,7 +317,7 @@ function printCoreUI {
 				fi
 				;;
 
-			12)
+			42)
 				read -p " ${green}Please enter a package name for purge: ${normal}" search
 				if [[ $rootUser==false ]]; then
 					executeAPTCommand "sudo apt purge $search"
@@ -273,7 +326,7 @@ function printCoreUI {
 				fi
 				;;
 
-			13)
+			43)
 				if [[ $rootUser==false ]]; then
 					executeAPTCommand "sudo apt-get autoremove"
 				else
@@ -294,7 +347,13 @@ function printCoreUI {
 				;;
 
 	   		[qQ])
+				clear
 				exit;;
+
+			*)
+				#printf " ${bold}${red}ERROR${normal}\tInvalid input.\n"
+				#pause
+				;;
 		esac
 	done
 }
